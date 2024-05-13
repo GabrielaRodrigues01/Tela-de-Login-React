8c5b50871f548706052c94b22d8113c1d50a6385
Gabriela Rodrigues <78853618+GabrielaRodrigues01@users.noreply.github.com>
2024-05-13 00:31:39 -0300

    Alterações css

--- src/assets/Components/Login/Login.jsx
+++ src/assets/Components/Login/Login.jsx
@@ -21,14 +21,20 @@ const Login = () => {
         <div className="container">
             <form onSubmit={envioForm}>
                 <h1>Acesse o sistema</h1>
-                <div>
-                    <input type="email" placeholder="E-mail" 
+                <div className="input-field">
+                    <input 
+                    type="email" 
+                    placeholder="E-mail" 
+                    required
                     onChange={(e) => setUsername(e.target.value)}
                      />
                     <FaUser className="icon" />
                 </div>
-                <div>
-                    <input type="password" placeholder="Senha"
+
+                <div className="input-field">
+                    <input 
+                    type="password" 
+                    placeholder="Senha"
                     onChange={(e) => setPassword(e.target.value)}
                      />
                     <FaLock className="icon" />
